ba8f6d2a42ea2033a6a16e85873cd4b5e1f6caed
yuu <hayashi.yuu@gmail.com>
2019-01-15 22:25:35 +0900

    fixed:

--- fuel.js
+++ fuel.js
@@ -35,19 +35,19 @@ var map = L.map('map',{
     });
 
     function showLatLng (e) {
-        location.href = "/unMapped/api/kmz/fuel?latlng="+ e.latlng +"&km=1";
+        location.href = "http://surveyor.mydns.jp/unMapped/api/kmz/fuel?latlng="+ e.latlng +"&km=1";
     }
     function showLatLng3 (e) {
-        location.href = "/unMapped/api/kmz/fuel?latlng="+ e.latlng +"&km=3";
+        location.href = "http://surveyor.mydns.jp/unMapped/api/kmz/fuel?latlng="+ e.latlng +"&km=3";
     }
     function showLatLng5 (e) {
-        location.href = "/unMapped/api/kmz/fuel?latlng="+ e.latlng +"&km=5";
+        location.href = "http://surveyor.mydns.jp/unMapped/api/kmz/fuel?latlng="+ e.latlng +"&km=5";
     }
     function showLatLng10 (e) {
-        location.href = "/unMapped/api/kmz/fuel?latlng="+ e.latlng +"&km=10";
+        location.href = "http://surveyor.mydns.jp/unMapped/api/kmz/fuel?latlng="+ e.latlng +"&km=10";
     }
     function showLatLng20 (e) {
-        location.href = "/unMapped/api/kmz/fuel?latlng="+ e.latlng +"&km=20";
+        location.href = "http://surveyor.mydns.jp/unMapped/api/kmz/fuel?latlng="+ e.latlng +"&km=20";
     }
 
     function setCenterMap (e) {
